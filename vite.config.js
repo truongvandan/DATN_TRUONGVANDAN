b6eecb87f7b8abe81e712cb7d026c23c65ecdf78
truongvandan <dantruong@DESKTOP-B0D0J2Q>
2023-04-16 22:48:32 +0700

    Test vite config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve:{
     alias: [
-      { find: '@', replacement: './src' },
+      { find: '@', replacement: path.resolve(__dirname, './src') },
     ],
   },
   plugins: [react(), tailwindcss()],
