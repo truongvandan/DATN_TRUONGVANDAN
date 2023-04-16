399238fb936ed9ac3855a99942a29b281ce9bb75
truongvandan <dantruong@DESKTOP-B0D0J2Q>
2023-04-16 23:29:59 +0700

    Enable debug build mode

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve:{
-    alias: 
-    {
-      '@' : '/src',
+    alias: {
+      '@' : path.resolve(__dirname, './src'),
     },
   },
   plugins: [react(), tailwindcss()],
