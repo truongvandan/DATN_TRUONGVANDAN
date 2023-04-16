5105e6f181b54aa24a470c1b4f8ac2aa9e1bb3a9
truongvandan <dantruong@DESKTOP-B0D0J2Q>
2023-04-16 20:49:30 +0700

    vacxin project

--- src/components/AuthController/index.jsx
+++ src/components/AuthController/index.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import { Navigate } from "react-router-dom";
 import { useAuth } from '../../hooks/useAuth';
+import { ROUTES } from '../../contants/router';
 
 function AuthController() {
     const [session] = useAuth();
 
     if(!session) {
-        return <Navigate to="/login"/>;
+        return <Navigate to={ROUTES.login} />;
     }
 
     return (
