e353276b33842c92a2e3b067f5e63dea9922b810
truongvandan <dantruong@DESKTOP-B0D0J2Q>
2023-04-16 22:43:26 +0700

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss'
-import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve:{
-    alias:{
-      '@' : path.resolve(__dirname, './src')
-    },
+    alias: [
+      { find: '@', replacement: './src' },
+    ],
   },
   plugins: [react(), tailwindcss()],
 })
